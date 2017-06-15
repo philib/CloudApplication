04e4e06d18a721619ff0dae0e53fd651eb2643c9
Johannes Burghardt <Johannes.Burghardt@iteratec.de>
2017-06-15 13:42:20 +0200

    conditional serve baby

--- BotFrontend/server.js
+++ BotFrontend/server.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     app = express();
 app.use(express.static('www'));
+
+app.get('/', function (req, res) {
+	res.sendfile('www/index.html')
+});
 app.get('/:tenantId', function (req, res) {
 
-    //TODO gibts diese tenantID? Falls ja sendfile, falls nein redirect zu google.com
-    res.sendfile('www/index.html');
+    res.sendfile('www/assets/foo.html');
 })
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
