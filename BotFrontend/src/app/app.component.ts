c06e82d4b3a0e5ce0a8e8a9665eb390be10dbf71
Ína Kuhn <raina.bertolini@gmail.com>
2017-06-19 22:06:44 +0200

    Addin BotFrontend help event

--- BotFrontend/src/app/app.component.ts
+++ BotFrontend/src/app/app.component.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { HomePage } from '../pages/home/home';
+import {Component} from "@angular/core";
+import {Platform} from "ionic-angular";
+import {HomePage} from "../pages/home/home";
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage
+    rootPage: any = HomePage
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-    });
-  }
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+        });
+    }
 }
