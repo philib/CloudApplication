7ea7e34fbae9d7225cefe993f080437dafaabc27
Johannes Burghardt <Johannes.Burghardt@iteratec.de>
2017-06-19 13:16:28 +0200

    Add statistics to bill page

--- TenantFrontend/src/pages/bill/bill.ts
+++ TenantFrontend/src/pages/bill/bill.ts
@@ -10,21 +10,47 @@ import {Http} from "@angular/http";
  Ionic pages and navigation.
  */
 @Component({
-  selector: 'page-bill',
-  templateUrl: 'bill.html'
+    selector: 'page-bill',
+    templateUrl: 'bill.html'
 })
 export class BillPage {
 
-  constructor(public navParams: NavParams, public http: Http, public alertCtrl: AlertController,
-              public baseUrl: BaseUrl, public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController) {
-  }
+    private Intent_News = 0
+    private No_Intent = 0
+    private Intent_CarRetailer = 0
+    private Intent_CarWorkshop = 0
+    private Intent_CarTypes = 0
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BillPage');
-  }
-  pay(){
-    
+    constructor(public navParams: NavParams, public http: Http, public alertCtrl: AlertController,
+                public baseUrl: BaseUrl, public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController) {
 
-  }
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad BillPage')
+        this.parseStatistics()
+    }
+
+    parseStatistics() {
+        this.Intent_News = 0
+        this.No_Intent = 0
+        this.Intent_CarRetailer = 0
+        this.Intent_CarWorkshop = 0
+        this.Intent_CarTypes = 0
+        this.navParams.data.tenant.configuration.chatHistory.map(x => {
+            let intent = JSON.parse(x.botOutput).intent
+            if (intent === 'No_Intent') this.No_Intent += 1
+            else if (intent === 'Intent_News') this.Intent_News += 1
+            else if (intent === 'Intent_CarRetailer') this.Intent_CarRetailer += 1
+            else if (intent === 'Intent_CarWorkshop') this.Intent_CarWorkshop += 1
+            else if (intent === 'Intent_CarTypes') this.Intent_CarTypes += 1
+            else if (intent === 'Intent_News') this.Intent_News += 1
+        })
+    }
+
+    pay() {
+
+
+    }
 
 }
