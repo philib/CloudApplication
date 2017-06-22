d6c404afe62122b48183a2a7aa3481919da536b5
Johannes Burghardt <Johannes.Burghardt@iteratec.de>
2017-06-22 14:43:47 +0200

    update scripts

--- eb-setenv.sh
+++ eb-setenv.sh
@@ -4,34 +4,29 @@ source ./env
 if [ "${1}" = "authService" ]; then
     echo deploying AuthService
 	cd MicroServices/AuthService
-	npm install
-	eb deploy
+	eb setenv JWT_SECRET=$JWT_SECRET DB_ENDPOINT=$DB_ENDPOINT PORT=$AuthService_PORT
 fi
 
 if [ "${1}" = "botService" ]; then
     echo deploying BotService
 	cd MicroServices/BotService
-	npm install
-	eb deploy
+	eb setenv AWS_LEX_ACCESS=$AWS_LEX_ACCESS AWS_LEX_SECRET=$AWS_LEX_SECRET DB_ENDPOINT=$DB_ENDPOINT JWT_SECRET=$JWT_SECRET PORT=$BotService_PORT
 fi
 
 if [ "${1}" = "tenantService" ]; then
     echo deploying TenantService
 	cd MicroServices/TenantServicee
-	npm install
-	eb deploy
+	eb setenv DB_ENDPOINT=$DB_ENDPOINT JWT_SECRET=$JWT_SECRET PORT=$TenantService_PORT
 fi
 
 if [ "${1}" = "BotFrontend" ]; then
     echo deploying BotFrontend
 	cd BotFrontend
-	npm install
-	eb deploy
+	eb setenv NODE_ENV=prod
 fi
 
 if [ "${1}" = "TenantFrontend" ]; then
     echo deploying TenantFrontend
 	cd TenantFrontend
-	npm install
-	eb deploy
+	eb setenv NODE_ENV=prod
 fi
